4e9d4507fd5239b9eed442d440cea0385a5d33ef
ptlmeet27 <20124040@nuv.ac.in>
2021-10-24 22:56:43 +0530

    Update orderAgnosticBS.java

--- Code/Day-1/src/com/meet/orderAgnosticBS.java
+++ Code/Day-1/src/com/meet/orderAgnosticBS.java
@@ -4,7 +4,7 @@ public class orderAgnosticBS {
     public static void main(String[] args) {
         //        int[] arr = {-18, -12, -4, 0, 2, 3, 4, 15, 16, 18, 22, 45, 89};
         int[] arr = {99, 80, 75, 22, 11, 10, 5, 2, -3};
-        int target = 22;
+        int target = 75;
         int ans = orderAgnosticBS(arr, target);
             System.out.println(ans);
     }
